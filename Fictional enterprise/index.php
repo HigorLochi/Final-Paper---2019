110317991fc3c5128e5ad375e4aecf909906f32d
HigorLochi <47319811+Musume1061@users.noreply.github.com>
2020-06-06 22:30:04 -0300

    Add files via upload

---
+++ Fictional enterprise/index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE HTML>
+<!--
+	Solid State by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>HL Softwares - Praise the Softwares</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="shortcut icon" type="image/x-icon" href="images/LogoEX.png">
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">HL Softwares - Praise the Softwares</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="#banner">Inicio</a></li>
+								<li><a href="#one">Missão, visão e valores</a></li>
+								<li><a href="#five">Serviços</a></li>
+								<li><a href="#four">Sobre a HL Softwares</a></li>
+								<li><a href="#footer">Entre em contato</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="image"><img style="width: 80%;" src="images/intro.png"></div>
+							<h2></h2>
+							<p>Empresa responsável pelo desenvolvimento de Softwares e Websites</p>
+						</div>
+					</section>
+
+				<!-- Wrapper -->
+					<section id="wrapper">
+
+						<!-- One -->
+							<section id="one" class="wrapper spotlight style1">
+								<div class="inner">
+									<a href="#" class="image"><img src="images/missao.png" alt="" /></a>
+									<div class="content">
+										<h2 class="major">Missão</h2>
+										<p>A principal missão da HL Softwares é desenvolver e fornecer softwares para pequenas empresas, e principalmente se desenvolverem em conhecimento e tecnologia.</p>
+										<!--a href="#" class="special">Learn more</a>-->
+									</div>
+								</div>
+							</section>
+
+						<!-- Two -->
+							<section id="two" class="wrapper alt spotlight style2">
+								<div class="inner">
+									<a href="#" class="image"><img src="images/visao.png" alt="" /></a>
+									<div class="content">
+										<h2 class="major">Visão</h2>
+										<p>Pretendemos tornar a HL Softwares em uma das melhores empresas no ramo da tecnologia para desenvolvermos qualquer tipo de serviços técnicos, desde montar softwares até desenvolver marcas famosas e utilitárias.</p>
+										
+									</div>
+								</div>
+							</section>
+
+						<!-- Three -->
+							<section id="three" class="wrapper spotlight style3">
+								<div class="inner">
+									<a href="#" class="image"><img src="images/valores.png" alt="" /></a>
+									<div class="content">
+										<h2 class="major">Valores</h2>
+										<p>
+											•	Visamos o comprometimento do funcionário na realização do trabalho.</br>
+                                            •	Desejamos respeitar e criar parcerias.</br>
+                                            •	Queremos oferecer o melhor para clientes.
+										</p>
+										
+									</div>
+								</div>
+							</section>
+
+						<!-- Four -->
+							<section id="five" class="wrapper alt style1">
+								<div class="inner">
+									<h2 class="major">Serviços</h2>
+									<p>Nossos serviços,atualmente, são realizados funcionários, estes que criam websites promocionais ou institucionais, através da linguagem HTML, CSS e PHP ou programas locais utilizando a linguagem C++. Caso você queira se juntar a nossa equipe, vá a aba <a href="#footer">Entre em contato</a> e mande um e-mail com suas informações currículares, nós retornaremos sua mensagem mostrando se há vagas compatíveis com suas habilidades.</p>
+									<h5>Utilizando HTML, CSS e PHP</h5>
+									<p>A base de nossos websites é HTML, e logicamente utilizando CSS para modificar a aparência deles, porém, em alguns sites é necessário um controle de dados em um banco de dados, e para isso utilizamos a linguagem PHP.</p>
+									<div class="box alt">
+							      		<div class="row gtr-uniform">
+								            <div class="col-6"><span class="image fit"><img src="images/html3.jpg" alt="" /></span></div>
+									    </div>
+									</div>
+									<h5>Utilizando C++</h5>
+									<p>Os programas locais realizados são feitos utlizando a linguagem C# no aplicativo <a target="new_blank" href="https://visualstudio.microsoft.com/pt-br/?rr=https%3A%2F%2Fwww.google.com%2F">Visual Studio</a>, e são realizados com a função do controle de dados, estes programas que são oferecidos pela HL Softwares para pequenas empresas que necessitam de sistemas.</p>
+									<div class="box alt">
+							      		<div class="row gtr-uniform">
+								            <div class="col-6"><span class="image fit"><img src="images/C.jpg" alt="" /></span></div>
+									    </div>
+									</div>
+									<h2 id="four" class="major">Sobre nós</h2>
+									<p>A HL Softwares foi formada por dois estudantes do <a href="https://www.unasp.br/" target="new_blank">UNASP</a>, Higor Aparecido Lochi de Almeida e Lucas Passoni de Lima, durante o 3º do ensino médio, com o objetivo de aprender mais sobre programação e de se tornarem os melhores desenvolvedores na área da tecnologia da informação. A formação da HL Softwares foi concluída com o primeiro projeto terminado: um site para a <a href="https://www.hiatec.com.br/" target="new_blank">Hiatec</a> que foi completado com a finalidade de melhorias estéticas e de um sistema web próprio para cadastros e manipulação de dados, como informações de clientes e suas respectivas máquinas, além de registrar ordens de serviços.</p>
+									<p>Nós estamos contratando pessoas para melhorar nosso serviços e fazer com que nossa missão seja completa, assim de acordo com nossos valore estabelecidos. Atualmente nós trabalhamos com a venda de serviços relacionados ao desenvolvimento de programas básicos e websites (utilizando ou não banco de dados). O prédio oficial da HL Softwares não foi designada ainda, então os serviços e trabalhos serão realizados nas determinadas residências dos programadores.
+									</p>
+									<section class="features">
+										<article>
+											<a class="image"><img src="images/Lochi.jpg" alt="" /></a>
+											<h3 class="major">Higor Aparecido Lochi de Almeida</h3>
+											<p>Presidente da HL Softwares</p>	
+											<!--<a href="info.html" class="special">Saiba mais sobre nós</a>-->
+										</article>
+										<article>
+											<a class="image"><img src="images/Passoni.jpeg" alt="" /></a>
+											<h3 class="major">Lucas Passoni de Lima</h3>
+											<p>CEO</p>									
+										</article>
+										<!--<article>
+											<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+											<h3 class="major">Ante fermentum</h3>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
+											<a href="#" class="special">Learn more</a>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+											<h3 class="major">Fusce consequat</h3>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
+											<a href="#" class="special">Learn more</a>
+										</article>
+									</section>
+									<ul class="actions">
+										<li><a href="#" class="button">Browse All</a></li>
+									</ul>-->
+								</div>
+							</section>
+
+					</section>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Entre em contato</h2>
+							<p>Mande uma mensagem para o nosso email e responderemos o mais rápido possivel, e verifique nossas redes sociais.</p>
+							<form method="POST" action="contato.php" target="">
+								<div class="fields">
+									<div class="field">
+										<label>Nome</label>
+										<input type="text" name="Nome" id="name" />
+									</div>
+									<div class="field">
+										<label>Email</label>
+										<input type="email" name="Email" id="email" />
+									</div>
+									<div class="field">
+										<label>Mensagem</label>
+										<textarea name="Mensagem" id="message" rows="4"></textarea>
+									</div>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" value="Enviar Mensagem"/></li>
+								</ul>
+							</form>
+							<ul class="contact">
+								<label>Localização para contato</label>
+								<div class="mapouter"><div class="gmap_canvas"><iframe width="500" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=rua%20virginia%20ortolan%20138&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://ultimatewp.net">UltimateWP</a></div><style>.mapouter{position:relative;text-align:right;height:300px;width:500px;}.gmap_canvas {overflow:hidden;background:none!important;height:300px;width:500px;}</style></div>
+
+								<!--<li class="icon solid fa-home">
+									HL Softwares
+								</li>-->
+								<li class="icon solid fa-phone">(19) 98167-7803</li>
+								<li class="icon solid fa-envelope">hlsoftwaresoficial@gmail.com</li>
+								<!--<li class="icon brands fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>-->
+					
+								<!--<li class="icon brands fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>-->
+							</ul>
+							<ul class="copyright">
+								<!--<li>&copy; Untitled Inc. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>-->
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
